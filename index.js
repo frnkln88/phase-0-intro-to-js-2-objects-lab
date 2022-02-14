88fe76976db910d8e7bf16dd9bf3fcd3ff031036
Frank Lin <franklin88@gmail.com>
2022-02-14 08:43:57 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Frank',
+    streetAddress: 'Section 7'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updated = {...employee}
+    updated[key] = value;
+    return updated
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const original = {...employee}
+    delete employee[key]
+    return employee;
+}
